7ba1325f437c7499fc378e34eb3412008bd390cc
olegivo@gmail.com <olegivo@gmail.com>
2011-08-14 17:04:04 +0000

    Изменены нэймспэйсы

--- MeloManager/App.xaml.cs
+++ MeloManager/App.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
-namespace MeloManager
+namespace Oleg_ivo.MeloManager
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MeloManager/MainWindow.xaml.cs
+++ MeloManager/MainWindow.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace MeloManager
+namespace Oleg_ivo.MeloManager
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
