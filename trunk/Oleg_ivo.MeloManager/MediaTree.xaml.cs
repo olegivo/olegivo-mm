36029487c6b5c1d6048fa66613f1e82259997972
olegivo@gmail.com <olegivo@gmail.com@4d253a49-06d9-b54c-a486-ee3a4acd89b2>
2011-08-15 22:08:10 +0000

    Главная форма, MediaList, MediaTree

---
+++ trunk/Oleg_ivo.MeloManager/MediaTree.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows.Controls;
+
+namespace Oleg_ivo.MeloManager
+{
+    /// <summary>
+    /// Interaction logic for MediaTree.xaml
+    /// </summary>
+    public partial class MediaTree : UserControl
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MediaTree()
+        {
+            InitializeComponent();
+
+            InitDatasource();
+        }
+
+        private void InitDatasource()
+        {
+            var ds = new[]
+                         {
+                             new {Name = "Плейлисты", ID = 0, ParentID = 0},
+                                new {Name = "Плейлист1", ID = 1, ParentID = 0}, 
+                                    new {Name = "Файл11", ID = 11, ParentID = 1}, 
+                                    new {Name = "Файл12", ID = 12, ParentID = 1},
+                                new {Name = "Плейлист2", ID = 2, ParentID = 0}, 
+                                    new {Name = "Файл21", ID = 21, ParentID = 2}, 
+                                    new {Name = "Файл22", ID = 22, ParentID = 2}
+                         };
+
+            //tree.Columns.AddField("Name");
+            tree.DataSource = ds;
+            tree.ExpandAll();}
+    }
+}
