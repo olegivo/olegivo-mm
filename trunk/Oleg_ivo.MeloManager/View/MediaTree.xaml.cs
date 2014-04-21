1011be9d992c2b46a01a14bfdef92917922435d1
olegivo@gmail.com <olegivo@gmail.com@4d253a49-06d9-b54c-a486-ee3a4acd89b2>
2014-04-21 03:46:32 +0000

    Фильтрация дерева: автоматическое разворачивание всех узлов если в фильтре что-то есть и наоборот

--- trunk/Oleg_ivo.MeloManager/View/MediaTree.xaml.cs
+++ trunk/Oleg_ivo.MeloManager/View/MediaTree.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Oleg_ivo.Base.Autofac.DependencyInjection;
+﻿using System.Windows;
 using Oleg_ivo.MeloManager.ViewModel;
 using Telerik.Windows.Controls;
 
@@ -17,11 +17,27 @@ namespace Oleg_ivo.MeloManager.View
             InitializeComponent();
         }
 
-        [Dependency(Required = true)]
+        /*public static readonly DependencyProperty IntValueProperty =
+        DependencyProperty.Register("MediaTreeViewModel", typeof(int), typeof(MediaTreeViewModel), new UIPropertyMetadata(1, OnIntValuePropertyChanged));
+        private static void OnIntValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            MediaTreeViewModel newPropertyValue = (MediaTreeViewModel)e.NewValue;
+            MediaTree instance = (MediaTree)d;
+            // Perform callback action.
+        }*/
+
         public MediaTreeViewModel ViewModel
         {
             get { return (MediaTreeViewModel)DataContext; }
-            set { DataContext = value; }
+            /*set
+            {
+                if (DataContext == value) return;
+                if (ViewModel != null)
+                    ViewModel.PropertyChanged -= ViewModel_PropertyChanged;
+                DataContext = value;
+                if (ViewModel != null)
+                    ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+            }*/
         }
 
         private void RadTreeView_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
@@ -36,7 +52,7 @@ namespace Oleg_ivo.MeloManager.View
                 path = currentItem.Name + path;
                 currentItem = currentItem.Parent;
             }
-            var treeViewItem = radTreeView.GetItemByPath(path);
+            //var treeViewItem = radTreeView.GetItemByPath(path);
             radTreeView.BringPathIntoView(path);
         }
 
@@ -61,6 +77,27 @@ namespace Oleg_ivo.MeloManager.View
             //tree.ExpandAll();
         }
         */
+
+        private void MediaTree_OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (ViewModel != null)
+                ViewModel.PropertyChanged += ViewModel_PropertyChanged;
+        }
+
+        void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "NameFilter")
+            {
+                if (string.IsNullOrEmpty(ViewModel.NameFilter))
+                {
+                    tree.CollapseAll();
+                }
+                else
+                {
+                    tree.ExpandAll();
+                }
+            }
+        }
 
     }
 }
