7d9b10b6ca6bada528cba0f6a0d60d8c3c8d7c8a
olegivo@gmail.com <olegivo@gmail.com@4d253a49-06d9-b54c-a486-ee3a4acd89b2>
2014-04-23 18:10:33 +0000

    Добавлен проект БД

---
+++ trunk/Oleg_ivo.MeloManager.Database/dbo/Tables/Files.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Files] (
+    [Id]                       BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Drive]                    VARCHAR (50)  NOT NULL,
+    [Path]                     VARCHAR (255) NOT NULL,
+    [Filename]                 VARCHAR (255) NOT NULL,
+    [Extention]                VARCHAR (10)  NOT NULL,
+    [FullFileName]             AS            ((([Path]+'\')+[FileNameWithoutExtension])+[Extention]),
+    [FileNameWithoutExtension] VARCHAR (255) NOT NULL,
+    CONSTRAINT [PK_Files] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ trunk/Oleg_ivo.MeloManager.Database/dbo/Tables/MediaContainersParentChilds.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[MediaContainersParentChilds] (
+    [Id]       BIGINT IDENTITY (1, 1) NOT NULL,
+    [ParentId] BIGINT NOT NULL,
+    [ChildId]  BIGINT NOT NULL,
+    CONSTRAINT [PK_MediaContainersInMediaContainers] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_MediaContainersParentChild_MediaContainer_Child] FOREIGN KEY ([ChildId]) REFERENCES [dbo].[MediaContainers] ([Id]),
+    CONSTRAINT [FK_MediaContainersParentChild_MediaContainer_Parent] FOREIGN KEY ([ParentId]) REFERENCES [dbo].[MediaContainers] ([Id]),
+    CONSTRAINT [UK_MediaContainersParentChilds] UNIQUE NONCLUSTERED ([ParentId] ASC, [ChildId] ASC)
+);
+
