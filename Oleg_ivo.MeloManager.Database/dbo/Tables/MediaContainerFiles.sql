4bfcd743db16f24989a8f7c970a592e71a30f70d
olegivo@gmail.com <olegivo@gmail.com>
2014-04-23 18:10:33 +0000

    Добавлен проект БД

---
+++ Oleg_ivo.MeloManager.Database/dbo/Tables/MediaContainerFiles.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[MediaContainerFiles] (
+    [Id]               BIGINT IDENTITY (1, 1) NOT NULL,
+    [MediaContainerId] BIGINT NOT NULL,
+    [FileId]           BIGINT NOT NULL,
+    CONSTRAINT [PK_MediaContainerFiles] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_MediaContainerFiles_Files] FOREIGN KEY ([FileId]) REFERENCES [dbo].[Files] ([Id]) ON UPDATE CASCADE,
+    CONSTRAINT [FK_MediaContainerFiles_MediaContainers] FOREIGN KEY ([MediaContainerId]) REFERENCES [dbo].[MediaContainers] ([Id]) ON UPDATE CASCADE
+);
+
---
+++ Oleg_ivo.MeloManager.Database/dbo/Tables/MediaContainers.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[MediaContainers] (
+    [Id]         BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Type]       VARCHAR (255) NOT NULL,
+    [Name]       VARCHAR (255) NOT NULL,
+    [IsRepaired] BIT           NOT NULL,
+    [IsRoot]     BIT           CONSTRAINT [DF_MediaContainers_IsRoot] DEFAULT ((1)) NOT NULL,
+    CONSTRAINT [PK_MediaContainer] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Находится ли контейнер в корне иерархии. Контейнер может находиться и в корне, и быть вложенным одновременно.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'MediaContainers', @level2type = N'COLUMN', @level2name = N'IsRoot';
+
