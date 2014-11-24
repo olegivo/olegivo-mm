e864431f52ccc4dd53baf4ecf2ed3f56063d0f33
olegivo@gmail.com <olegivo@gmail.com@4d253a49-06d9-b54c-a486-ee3a4acd89b2>
2014-11-24 14:23:07 +0000

    Files: VARCHAR->NVARCHAR FullFileName рассчитывается с помощью новой функции GetFullFulename Добавлен уникальный индекс по FullFileName Добавлена таблица TMediaContainerType

---
+++ trunk/Oleg_ivo.MeloManager.Database/dbo/Tables/TMediaContainerType.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TMediaContainerType] (
+    [MediaContainerType] VARCHAR (255)  NOT NULL,
+    [Description]        NVARCHAR (200) NOT NULL,
+    CONSTRAINT [PK_TMediaContainerType] PRIMARY KEY CLUSTERED ([MediaContainerType] ASC)
+);
+
