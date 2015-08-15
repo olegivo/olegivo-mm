e96ef334a76039dcdc13e3e5558a34aa8d68e7fe
olegivo <olegivo@gmail.com>
2015-08-15 16:23:43 +0300

    Рефакторинг MediaDataContext и IMediaCache
    Dictionary -> ConcurrentDictionary
    Факт существования файла теперь кешируется
    Приватизация свойств MediaDataContext, которые используются только внутри

--- Oleg_ivo.Melomanager.MediaObjects/IMediaCache.cs
+++ Oleg_ivo.Melomanager.MediaObjects/IMediaCache.cs
@@ -1,10 +1,7 @@
-using System.Collections.Generic;
-
 namespace Oleg_ivo.MeloManager.MediaObjects
 {
     public interface IMediaCache
     {
-        Dictionary<string, MediaFile> MediaFilesCache { get; set; }
         void RefreshCache();
         File GetOrAddCachedFile(string fullFilename);
         MediaFile GetOrAddCachedMediaFile(string filename);
--- Oleg_ivo.Melomanager.MediaObjects/MediaDataContext.cs
+++ Oleg_ivo.Melomanager.MediaObjects/MediaDataContext.cs
@@ -1,11 +1,17 @@
-using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using NLog;
 using Oleg_ivo.Base.Extensions;
+using Oleg_ivo.Tools.Utils;
 
 namespace Oleg_ivo.MeloManager.MediaObjects
 {
     partial class MediaDataContext : IMediaCache
     {
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         public Category CreateCategory()
         {
             var category = new Category();
@@ -44,19 +50,37 @@ namespace Oleg_ivo.MeloManager.MediaObjects
             MediaContainersParentChilds.DeleteOnSubmit(childRelation);
         }
 
-        public Dictionary<string, File> FilesCache { get; set; }
+        private class FileExt
+        {
+            public FileExt(File file)
+            {
+                File = file;
+                RefreshExists();
+            }
 
-        public Dictionary<string, MediaFile> MediaFilesCache { get; set; }
+            public readonly File File;
+            public bool Exists;
 
-        public Dictionary<string, Playlist> PlaylistsCache { get; set; }
+            public void RefreshExists()
+            {
+                Exists = System.IO.File.Exists(File.FullFileName);
+            }
+        }
+
+        private ConcurrentDictionary<string, FileExt> filesCache;
+        private ConcurrentDictionary<string, MediaFile> mediaFilesCache;
+        private ConcurrentDictionary<string, Playlist> playlistsCache;
 
         partial void OnCreated()
         {
-            RefreshCache();
+            using (new ElapsedAction(elapsed => log.Debug("Caches filled in {0}", elapsed))) RefreshCache();
         }
+
+        
 
         public void RefreshCache()
         {
+            MediaContainers.Load();
             var d =
                 from mediaContainer in MediaContainers.Where(mc => mc is MediaFile || mc is Playlist)
                 join mcf in MediaContainerFiles on mediaContainer equals mcf.MediaContainer
@@ -64,7 +88,7 @@ namespace Oleg_ivo.MeloManager.MediaObjects
                 select
                     new
                     {
-                        file.FullFileName,
+                        FullFileName = file.FullFileName.ToLower(),
                         MediaFile = mediaContainer as MediaFile,
                         Playlist = mediaContainer as Playlist,
                         file
@@ -77,9 +101,17 @@ namespace Oleg_ivo.MeloManager.MediaObjects
             //    mediaFilesCache.Add(arg.FullFileName, arg.mediaFile);
             //}
 
-            MediaFilesCache = d.Where(arg => arg.MediaFile!=null).ToDictionary(arg => arg.FullFileName.ToLower(), arg => arg.MediaFile);
+            mediaFilesCache =
+                new ConcurrentDictionary<string, MediaFile>(
+                    d.Where(arg => arg.MediaFile != null)
+                        .AsParallel()
+                        .ToDictionary(arg => arg.FullFileName, arg => arg.MediaFile));
             //PlaylistsCache = d.Where(arg => arg.Playlist!=null).ToDictionary(arg => arg.FullFileName, arg => arg.Playlist);
-            FilesCache = d.ToDictionary(arg => arg.FullFileName.ToLower(), arg => arg.file);
+            //FilesCache = d.GroupBy(arg => arg.FullFileName.ToLower()).ToDictionary(g => g.Key, g => g.First().file);
+            //FilesCache = d.GroupBy(arg => arg.FullFileName.ToLower()).ToDictionary(g => g.Key, g => g.Single().file);
+            filesCache =
+                new ConcurrentDictionary<string, FileExt>(d.AsParallel()
+                    .ToDictionary(arg => arg.FullFileName, arg => new FileExt(arg.file)));
             /*
 
             mediaFilesCache =
@@ -91,50 +123,57 @@ namespace Oleg_ivo.MeloManager.MediaObjects
 
         public File GetOrAddCachedFile(string fullFilename)
         {
-            var key = fullFilename.ToLower();
-            var file = FilesCache.GetValueOrDefault(key);
-            if (file != null) return file;
+            return GetOrAddFile(fullFilename).File;
+        }
 
-            var fileName = System.IO.Path.GetFileName(fullFilename);
-            var fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fullFilename);
-            var extension = System.IO.Path.GetExtension(fullFilename);
-            var drive = System.IO.Path.GetPathRoot(fullFilename);
-            var path = System.IO.Path.GetDirectoryName(fullFilename);
-            file = new File
-            {
-                FullFileName = fullFilename,
-                Drive = drive,
-                Path = path,
-                Filename = fileName,
-                FileNameWithoutExtension = fileNameWithoutExtension,
-                Extention = extension
-            };
-            FilesCache.Add(key, file);
-            return file;
+        public bool GetOrAddFileExists(string fullFilename, bool refreshExists = false)
+        {
+            var fileExt = GetOrAddFile(fullFilename);
+            if (refreshExists) fileExt.RefreshExists();
+            return fileExt.Exists;
+        }
+
+        private FileExt GetOrAddFile(string fullFilename)
+        {
+            return filesCache.GetOrAdd(fullFilename.ToLower(),
+                key =>
+                {
+                    var fileName = Path.GetFileName(fullFilename);
+                    var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fullFilename);
+                    var extension = Path.GetExtension(fullFilename);
+                    var drive = Path.GetPathRoot(fullFilename);
+                    var path = Path.GetDirectoryName(fullFilename);
+                    return new FileExt(new File
+                    {
+                        FullFileName = fullFilename,
+                        Drive = drive,
+                        Path = path,
+                        Filename = fileName,
+                        FileNameWithoutExtension = fileNameWithoutExtension,
+                        Extention = extension
+                    });
+                });
         }
 
         public MediaFile GetOrAddCachedMediaFile(string filename)
         {
-            var key = filename.ToLower();
-            var mediaFile = MediaFilesCache.GetValueOrDefault(key);
-            if (mediaFile != null) return mediaFile;
+            return mediaFilesCache.GetOrAdd(filename.ToLower(),
+                key =>
+                {
+                    var mediaFile = mediaFilesCache.GetValueOrDefault(key);
+                    if (mediaFile != null) return mediaFile;
 
-            var file = GetOrAddCachedFile(filename);
-            mediaFile = new MediaFile {Name = file.Filename};
-            mediaFile.MediaContainerFiles.Add(new MediaContainerFile{File = file});
-            MediaFilesCache.Add(key, mediaFile);
-            return mediaFile;
+                    var file = GetOrAddCachedFile(filename);
+                    mediaFile = new MediaFile {Name = file.Filename};
+                    mediaFile.MediaContainerFiles.Add(new MediaContainerFile {File = file});
+                    return mediaFile;
+                });
         }
 
         public Playlist GetOrAddCachedPlaylist(string filename, string playlistName = null)
         {
-            filename = filename.ToLower();
-            var playlist = PlaylistsCache.GetValueOrDefault(filename);
-            if (playlist != null) return playlist;
-
-            playlist = new Playlist(filename, this) {Name = playlistName};
-            PlaylistsCache.Add(filename, playlist);
-            return playlist;
+            return playlistsCache.GetOrAdd(filename.ToLower(), 
+                key => new Playlist(filename, this) {Name = playlistName});
         }
     }
 }
